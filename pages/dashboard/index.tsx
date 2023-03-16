64e6735c7d78bdb7c3e97b6e43958a7080ffff98
unknown <pablo.mieres5@gmail.com>
2023-03-15 21:40:30 -0600

    Se agrego el layout inicial del Dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,18 +1,68 @@
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import styled from 'styled-components';
-import { Col, Container, Row, Button, Badge, Form, Stack, Card } from 'react-bootstrap';
-import Header from '../../src/components/Header'
+import styled from "styled-components";
+import classes from "../../styles/Dashboard.module.css";
+import {
+  Col,
+  Container,
+  Row,
+  Button,
+  Badge,
+  Form,
+  Stack,
+} from "react-bootstrap";
+import Header from "../../src/components/Header";
 import Footer from "../../src/components/Footer";
 import { TbWorld } from "react-icons/tb";
 import { BsBricks } from "react-icons/bs";
 import { HiArrowsUpDown } from "react-icons/hi2";
+import TokenCard from "../../src/components/TokenCard";
+import { BtnGreen } from "../../src/components/BtnGreen";
+import Card from "../../src/components/Card/Card";
 
 export default function Marketplace() {
-  return(
+  return (
     <>
       <Container className="mt-5">
-        
+        <div className={`${classes["grid-container"]}`}>
+          <div className={`${classes["grid-item1"]}`}>
+            <TokenCard type="grid"></TokenCard>
+          </div>
+
+          <Card className={`${classes["grid-item2"]}`}>
+            <h4 className={`${classes["grid-title"]}`}>
+              Fondos recaudados por Reserva Kaax
+            </h4>
+            <p className={`${classes["grid-text"]}`}>$5,000,000 USD</p>
+          </Card>
+
+          <Card className={`${classes["grid-item3"]}`}>
+            <h4 className={`${classes["grid-title"]}`}>
+              Número de inversionistas
+            </h4>
+            <p className={`${classes["grid-text"]}`}>46 inversionistas</p>
+          </Card>
+
+          <Card className={`${classes["grid-item4"]}`}>
+            <h4 className={`${classes["grid-title"]}`}>Tu inversión</h4>
+            <p className={`${classes["grid-text"]}`}>$40,000 USD</p>
+          </Card>
+
+          <Card className={`${classes["grid-item5"]}`}>
+            <h4 className={`${classes["grid-title"]}`}>Tus tokens</h4>
+            <p className={`${classes["grid-text"]}`}>2 vouchers</p>
+          </Card>
+
+          <Card className={`${classes["grid-item6"]}`}>GRAFICA</Card>
+
+          <Card className={`${classes["grid-item7"]}`}>
+            <h4 className={`${classes["grid-text"]}`}>Progreso de la obra</h4>
+          </Card>
+
+          <div className={`${classes["grid-item8"]}`}>
+            <BtnGreen>Volver a Invertir</BtnGreen>
+          </div>
+        </div>
       </Container>
     </>
   );
--- src/components/TokenCard.tsx
+++ src/components/TokenCard.tsx
@@ -1,27 +1,61 @@
 import { Container, Button, Row, Col, Form, ListGroup } from "react-bootstrap";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { BsStars } from "react-icons/bs";
 
 const CardContainer = styled(Container)`
   width: 60%;
   height: auto;
   border-radius: 2rem;
-  background: conic-gradient(#BEB024, #A9D9CE 40deg, #2A4038 150deg, #BEB024);
+  background: conic-gradient(#beb024, #a9d9ce 40deg, #2a4038 150deg, #beb024);
 `;
 
-export default function TokenCard() {
-  return(
-    <CardContainer className=" px-2 py-4 text-light">
-      <Container style={{ position: 'relative'}}>
-        <img src="https://images.unsplash.com/photo-1633114594701-52002208270f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80" className="img-fluid rounded" />
-        <BsStars className="my-2" size={32} style={{ position:'absolute', right:'5%', top:'2%' }} />
-      </Container>
-      <Container className="pt-3">
-        <p className="fw-bold m-0">Miembro Tulum</p>
-        <p className="fw-light">Token id: 1</p>
-        <p className="fw-bold m-0">Paulo Gonzales</p>
-        <p className="fw-light m-0">Fecha de maduración: 22/03/2023</p>
-      </Container>
-    </CardContainer>
-  );
+export default function TokenCard(props) {
+  if (props.type === "grid") {
+    return (
+      <CardContainer
+        className=" px-2 py-4 text-light"
+        style={{ width: "100%" }}
+      >
+        <Container style={{ position: "relative" }}>
+          <img
+            src="https://images.unsplash.com/photo-1633114594701-52002208270f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80"
+            className="img-fluid rounded"
+          />
+          <BsStars
+            className="my-2"
+            size={32}
+            style={{ position: "absolute", right: "5%", top: "2%" }}
+          />
+        </Container>
+        <Container className="pt-3">
+          <p className="fw-bold m-0">Miembro Tulum</p>
+          <p className="fw-light">Token id: 1</p>
+          <p className="fw-bold m-0">Paulo Gonzales</p>
+          <p className="fw-light m-0">Fecha de maduración: 22/03/2023</p>
+        </Container>
+      </CardContainer>
+    );
+  } else {
+    return (
+      <CardContainer className=" px-2 py-4 text-light">
+        <Container style={{ position: "relative" }}>
+          <img
+            src="https://images.unsplash.com/photo-1633114594701-52002208270f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80"
+            className="img-fluid rounded"
+          />
+          <BsStars
+            className="my-2"
+            size={32}
+            style={{ position: "absolute", right: "5%", top: "2%" }}
+          />
+        </Container>
+        <Container className="pt-3">
+          <p className="fw-bold m-0">Miembro Tulum</p>
+          <p className="fw-light">Token id: 1</p>
+          <p className="fw-bold m-0">Paulo Gonzales</p>
+          <p className="fw-light m-0">Fecha de maduración: 22/03/2023</p>
+        </Container>
+      </CardContainer>
+    );
+  }
 }
